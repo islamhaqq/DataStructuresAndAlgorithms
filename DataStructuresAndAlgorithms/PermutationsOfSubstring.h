5d1ac882b2a37fcd291b74896bb51f5de5109058
Syed Aman <syednashikaman@gmail.com>
2023-04-17 13:07:08 -0400

    Suboptimal solution of permutations of substring

---
+++ DataStructuresAndAlgorithms/PermutationsOfSubstring.h
@@ -0,0 +1,78 @@
+﻿/**
+  * Example: Given a smaller strings and a bigger string b, design an algorithm to find all permutations of the
+  * shorter string within the longer one. Print the location of each permutation. 
+  */
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class PermutationsOfSubstring
+{
+public:
+    // O(s! * b)
+    vector<int> FindPermutationSubstrings(const string& smaller, const string& bigger)
+    {
+        vector<int> locations;
+
+        // O(s!)
+        vector<string> permutations = GetPermutationsOfString(smaller);
+
+        // O(s! * b)
+        for (string permutation : permutations)
+        {
+            for (int i = 0; i < bigger.size(); i++)
+            {
+                if (permutation[0] == bigger[i])
+                {
+                    bool isSubstring = true;
+                    for (int j = 0; j < permutation.size(); j++)
+                    {
+                        if (permutation[j] != bigger[i + j])
+                        {
+                            isSubstring = false;
+                            break;
+                        }
+                    }
+                    if (isSubstring)
+                        locations.push_back(i);
+                }
+            }
+        }
+
+        
+        return locations;
+    }
+
+    // O(n!)
+    vector<string> GetPermutationsOfString(string Str)
+    {
+        if (Str.size() == 1 || Str.empty())
+            return vector<string>{Str};
+        
+        // 1. Each of the first characters
+        vector<string> permutations;
+        for (int i = 0; i < Str.size(); i++)
+        {
+            char firstCharacter = Str[i];
+
+            // 2. concatenated by the permutations of the rest
+            string theRest;
+            for (int j = 0; j < Str.size(); j++)
+            {
+                if (j == i) continue;
+                theRest += Str[j];
+            }
+            
+            vector<string> permutationsOfTheRest = GetPermutationsOfString(theRest);
+            for (string permutation : permutationsOfTheRest)
+            {
+                string permutationWithFirstCharacter;
+                permutationWithFirstCharacter += firstCharacter;
+                permutationWithFirstCharacter += permutation;
+                permutations.push_back(permutationWithFirstCharacter);
+            }
+        }
+        return permutations;
+    }
+};
---
+++ DataStructuresAndAlgorithms/PermutationsOfSubstring_Test.cpp
@@ -0,0 +1,52 @@
+﻿#include "PermutationsOfSubstring.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+class ParameterizedPermutationsOfSubstring : public ::testing::TestWithParam<tuple<string, string, vector<int>>> {};
+TEST_P(ParameterizedPermutationsOfSubstring, BruteForce)
+{
+    string smaller = std::get<0>(GetParam());
+    string bigger = std::get<1>(GetParam());
+    vector<int> expected = std::get<2>(GetParam());
+    vector<int> actual = PermutationsOfSubstring().FindPermutationSubstrings(smaller, bigger);
+    ASSERT_EQ(expected, actual);
+}
+INSTANTIATE_TEST_CASE_P(
+    PermutationsOfSubstring,
+    ParameterizedPermutationsOfSubstring,
+    ::testing::Values(
+        std::make_tuple("", "a", vector<int>{}),
+        std::make_tuple("a", "", vector<int>{}),
+        std::make_tuple("", "", vector<int>{}),
+        std::make_tuple("a", "a", vector<int>{0}),
+        std::make_tuple("a", "abc", vector<int>{0}),
+        std::make_tuple("a", "abca", vector<int>{0, 3}),
+        std::make_tuple("ab", "abca", vector<int>{0}),
+        std::make_tuple("ab", "abcab", vector<int>{0, 3}),
+        std::make_tuple("ab", "abcabcab", vector<int>{0, 3, 6}),
+        std::make_tuple("d", "abcabcabdddddd", vector<int>{8, 9, 10, 11, 12, 13}),
+        // other permutations of s
+        std::make_tuple("ab", "aba", vector<int>{0, 1})
+    )
+);
+
+class ParameterizedGetPermutationsOfString : public ::testing::TestWithParam<tuple<string, vector<string>>> {};
+TEST_P(ParameterizedGetPermutationsOfString, BruteForce)
+{
+    string str = std::get<0>(GetParam());
+    vector<string> expected = std::get<1>(GetParam());
+    vector<string> actual = PermutationsOfSubstring().GetPermutationsOfString(str);
+    ASSERT_EQ(expected, actual);
+}
+INSTANTIATE_TEST_CASE_P(
+    GetPermutationsOfString,
+    ParameterizedGetPermutationsOfString,
+    ::testing::Values(
+        std::make_tuple("", vector<string>{""}),
+        std::make_tuple("a", vector<string>{"a"}),
+        std::make_tuple("ab", vector<string>{"ab", "ba"}),
+        std::make_tuple("abc", vector<string>{"abc", "acb", "bac", "bca", "cab", "cba"}),
+        std::make_tuple("aab", vector<string>{"aab", "aba", "aab", "aba", "baa", "baa"})
+    )
+);
